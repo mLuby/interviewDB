4a420d107d660b76153892f528ea4e9bc64174ed
Mike Luby <Michael@mLuby.com>
2014-11-19 00:24:01 -0800

    refactor to bootstrap, no tiles

--- client/app.js
+++ client/app.js
@@ -1,27 +1,26 @@
-var app = angular.module('InterviewSecrets', ['icon', 'tile']);
+var app = angular.module('InterviewSecrets', []);
 
 app.value('Questions', [
-    {company: 'JPMC', type: 'factual', icon: 'book', prompt: 'what\'s a closure', answer: 'private var', conclusion: 'gj'}
+    {company: 'JPMC', type: 'factual', prompt: 'what\'s a closure', answer: 'private var', conclusion: 'gj'}
 ]);
 
 app.controller('AddQuestionController', function($scope, Questions){
   $scope.submitQuestion = function (company, type, prompt, answer, conclusion){
-    var icon; // deciding which font-awesome icon to use:
-    switch(type){
-      case 'coding': icon='keyboard-o'; break;
-      case 'whiteboarding': icon='pencil-square-o'; break;
-      case 'behavioral': icon='users'; break;
-      case 'factual': icon='book'; break;
-      default: icon='question-circle fa-spin'; //'other'
-    }
+    // Clear fields
+    $scope.type = '';
+    $scope.prompt = '';
+    $scope.answer = '';
+    $scope.conclusion = '';
+
+    // Create new question object.
     var question = {
       company: company,
       type: type,
-      icon: icon,
       prompt: prompt,
       answer: answer,
       conclusion: conclusion
     };
+
     Questions.push( question );
   };
 });
