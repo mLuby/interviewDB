2251fbf405e68abea51ddcdd9df463769e9234ef
Mike Luby <Michael@mLuby.com>
2014-11-20 00:15:41 -0800

    refactor to firebase

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-interviewSecrets
-================
+company has many question
+question has many company
+question has many answer
+answer has one question
+answer has one conclusion
+answer has one progressed
 
-Internal GlassDoor.com
+question1:
+  answer1: conclusion1: progressed1
+  answer2: conclusion2: progressed2
--- client/app.js
+++ client/app.js
@@ -1,11 +1,33 @@
-var app = angular.module('InterviewSecrets', []);
+var app = angular.module('interviewDB', ['firebase']);
 
-app.value('Questions', [
-    {company: 'JPMC', type: 'factual', prompt: 'what\'s a closure', answer: 'private var', conclusion: 'gj'}
-]);
+app.factory('DB', function($http, $firebase){
+  var ref = new Firebase('https://interviewDB.firebaseio.com/');
+  var sync = $firebase(ref);
 
-app.controller('AddQuestionController', function($scope, Questions){
-  $scope.submitQuestion = function (company, type, prompt, answer, conclusion){
+  var addQuestion = function(question){
+    sync.$push(question).then(function(newChildRef) {
+      console.log("added question");
+    });
+    // var list = $firebase(ref).$asArray();
+    // return list.$loaded(function(loadedList){
+    //   console.log('firebase loaded, deleting messageID', messageID);
+    //   return loadedList.$remove(loadedList.$getRecord(messageID));
+    // });
+  };
+
+  return {
+    push: addQuestion
+  }
+});
+
+
+app.value('Questions', []);
+
+// {company: 'Hooli', type: 'Factual', prompt: 'What\'s a closure?', answer: 'It\'s a private variable.', conclusion: 'Okay, can you give an example of when you used one?'},
+// {company: 'Pied Piper', type: 'Whiteboarding', prompt: 'Design an API to show all interactions from a single user.', answer: 'Let\'s capture user interaction via session tracking, then store that in a MongoDB.', conclusion: 'Okay, how can we make this faster?'}
+
+app.controller('QuestionController', function($scope, Questions, DB){
+  $scope.submitQuestion = function (company, date, type, prompt, answer, conclusion){
     // Clear fields
     $scope.type = '';
     $scope.prompt = '';
@@ -15,6 +37,7 @@ app.controller('AddQuestionController', function($scope, Questions){
     // Create new question object.
     var question = {
       company: company,
+      date: date,
       type: type,
       prompt: prompt,
       answer: answer,
@@ -22,9 +45,10 @@ app.controller('AddQuestionController', function($scope, Questions){
     };
 
     Questions.push( question );
+    DB.push( question );
   };
 });
 
-app.controller('ViewQuestionsController', function($scope, Questions){
+app.controller('ViewQuestionsController', function($scope, Questions, DB){
   $scope.questions = Questions;
 });
